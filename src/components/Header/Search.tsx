c1ab5c0998c155d16d52ec7250ef881d11353a76
Willian <roberg.willian@gmail.com>
2022-01-10 17:26:02 -0300

    chapter 4 done

--- src/components/Header/Search.tsx
+++ src/components/Header/Search.tsx
@@ -1,32 +1,42 @@
-import {Flex, Input, Icon} from '@chakra-ui/react'
-import { RiSearchLine } from 'react-icons/ri'
+import { Flex, Input, Icon } from "@chakra-ui/react";
+import { useRef, useState } from "react";
+import { RiSearchLine } from "react-icons/ri";
 
 export default function Search() {
-    return(
-        <Flex
-        as="label"
-        flex="1"
-        py="4"
-        px="8"
-        ml="6"
-        maxW={400}
-        alignSelf="center"
-        color="gray.200"
-        position="relative"
-        bg="gray.800"
-        borderRadius="full"
-      >
-        <Input
-          color="gray.50"
-          variant="unstyled"
-          px="4"
-          mr="4"
-          placeholder="Buscar na plataforma"
-          _placeholder={{
-            color: "gray.400",
-          }}
-        />
-        <Icon as={RiSearchLine} fontSize="20" />
-      </Flex>
-    )
+  // const [search, setSearch] = useState("");
+
+  const searchInputRef = useRef<HTMLInputElement>(null)
+
+  //debounce - evitar que seja feita a busca a cada letra digitada, sera executada depois de alguns segundos que o usuario parar de digitar
+
+  return (
+    <Flex
+      as="label"
+      flex="1"
+      py="4"
+      px="8"
+      ml="6"
+      maxW={400}
+      alignSelf="center"
+      color="gray.200"
+      position="relative"
+      bg="gray.800"
+      borderRadius="full"
+    >
+      <Input
+        color="gray.50"
+        variant="unstyled"
+        px="4"
+        mr="4"
+        placeholder="Buscar na plataforma"
+        _placeholder={{
+          color: "gray.400",
+        }}
+        // value={search}
+        // onChange={(e) => setSearch(e.target.value)}
+        ref={searchInputRef}
+      />
+      <Icon as={RiSearchLine} fontSize="20" />
+    </Flex>
+  );
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,29 @@
-import { Flex, Button, Stack, FormLabel, FormControl } from "@chakra-ui/react";
-import Input from "../components/Form/Input";
+import { Flex, Button, Stack } from "@chakra-ui/react";
+import { SubmitHandler, useForm } from "react-hook-form";
+import { Input } from "../components/Form/Input";
+import * as yup from "yup";
+import { yupResolver } from "@hookform/resolvers/yup";
+
+type SignInFormData = {
+  email: string;
+  password: string;
+};
+
+const signInFormSchema = yup.object().shape({
+  email: yup.string().required('E-mail obrigatorio').email('E-mail invalido'),
+  password: yup.string().required('Senha obrigatoria'),
+});
 
 export default function SignIn() {
+  const { register, handleSubmit, formState } = useForm({
+    resolver: yupResolver(signInFormSchema),
+  });
+
+  const handleSignIn: SubmitHandler<SignInFormData> = async (values, event) => {
+    await new Promise((resolve) => setTimeout(resolve, 2000));
+    console.log(values);
+  };
+
   return (
     <Flex w="100vw" h="100vh" align="center" justify="center">
       <Flex
@@ -12,12 +34,31 @@ export default function SignIn() {
         p="8"
         borderRadius={8}
         flexDir="column"
+        onSubmit={handleSubmit(handleSignIn)}
       >
         <Stack spacing="4">
-          <Input name="email" label="E-mail" type="email" />
-          <Input name="password" label="Password" type="password" />
+          <Input
+            name="email"
+            label="E-mail"
+            type="email"
+            error={formState.errors.email}
+            {...register("email")}
+          />
+          <Input
+            name="password"
+            label="Password"
+            type="password"
+            {...register("password")}
+            error={formState.errors.password}
+          />
         </Stack>
-        <Button type="submit" mt="6" colorScheme="pink" size="lg">
+        <Button
+          type="submit"
+          mt="6"
+          colorScheme="pink"
+          size="lg"
+          isLoading={formState.isSubmitting}
+        >
           Entrar
         </Button>
       </Flex>
