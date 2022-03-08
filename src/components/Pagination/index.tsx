f53136c6e7e6c09af502fb028feb480158e9a0ff
Willian <roberg.willian@gmail.com>
2022-03-08 16:46:49 -0300

    Pagination component

--- src/components/Pagination/index.tsx
+++ src/components/Pagination/index.tsx
@@ -1,7 +1,36 @@
 import { Button, Stack, Box } from "@chakra-ui/react";
 import { PaginationItem } from "./PaginationItem";
 
-export default function Pagination() {
+interface PaginationProps {
+  totalCountOfRegisters: number;
+  registersPerPage?: number;
+  currentPage?: number;
+  onPageChange: (page: number) => void;
+}
+
+const siblingsCount = 1;
+
+function generatePagesArray(from: number, to: number) {
+  return [...new Array(to - from)]
+    .map((_, index) => {
+      return from + index + 1;
+    })
+    .filter((page) => page > 0);
+}
+
+export default function Pagination({
+  totalCountOfRegisters,
+  registersPerPage = 10,
+  currentPage = 1,
+  onPageChange,
+}: PaginationProps) {
+  const lastPage = Math.floor(totalCountOfRegisters / registersPerPage);
+
+  const previousPages =
+    currentPage > 1 ? generatePagesArray(currentPage -1 - siblingsCount, currentPage - 1) : [];
+
+  const nextPages = currentPage < lastPage ? generatePagesArray(currentPage -1, Math.min(currentPage + siblingsCount, lastPage)) : [];
+
   return (
     <Stack
       direction={["column", "row"]}
@@ -14,14 +43,14 @@ export default function Pagination() {
         <strong>0</strong> - <strong>10</strong> de <strong>100</strong>
       </Box>
       <Stack direction="row" spacing="2">
-          <PaginationItem number={1} isCurrent/>
-          <PaginationItem number={2} />
-          <PaginationItem number={3} />
-          <PaginationItem number={4} />
-          <PaginationItem number={5} />
-          <PaginationItem number={6} />
-          <PaginationItem number={7} />
-          <PaginationItem number={8} />
+        <PaginationItem number={1} isCurrent />
+        <PaginationItem number={2} />
+        <PaginationItem number={3} />
+        <PaginationItem number={4} />
+        <PaginationItem number={5} />
+        <PaginationItem number={6} />
+        <PaginationItem number={7} />
+        <PaginationItem number={8} />
       </Stack>
     </Stack>
   );
